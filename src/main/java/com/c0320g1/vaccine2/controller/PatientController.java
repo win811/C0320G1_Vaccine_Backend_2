e26cbf03086efbef4af854f158c31f94f3353417
win811 <cuongwin811@gmail.com>
2020-10-13 20:55:25 +0700

    them nho

--- src/main/java/com/c0320g1/vaccine2/controller/PatientController.java
+++ src/main/java/com/c0320g1/vaccine2/controller/PatientController.java
@@ -4,7 +4,6 @@ import com.c0320g1.vaccine2.model.Patient;
 import com.c0320g1.vaccine2.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;

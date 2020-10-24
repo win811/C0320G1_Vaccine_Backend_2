77889492c0d14165dfa5be511658977a6caba989
win811 <cuongwin811@gmail.com>
2020-10-24 09:16:19 +0700

    B-Cuong 24/10 9:16

--- src/main/java/com/c0320g1/vaccine2/Vaccine2Application.java
+++ src/main/java/com/c0320g1/vaccine2/Vaccine2Application.java
@@ -2,8 +2,10 @@ package com.c0320g1.vaccine2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Vaccine2Application {
 
     public static void main(String[] args) {
